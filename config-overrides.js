4c70c5b0c4999052e9f9373d935edb57f3e5efa6
sylvanas <jzm362466464@126.com>
2020-12-06 17:27:06 +0800

    init pro

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: sylvanas
+ * @Date: 2020-12-02 22:30:00
+ * @LastEditors: sylvanas
+ * @LastEditTime: 2020-12-02 22:30:10
+ * @Description:
+ */
+const { override, addDecoratorsLegacy } = require('customize-cra')
+
+module.exports = override(addDecoratorsLegacy())
---
+++ src/stores/AppleStore.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: sylvanas
+ * @Date: 2020-12-06 17:22:16
+ * @LastEditors: sylvanas
+ * @LastEditTime: 2020-12-06 17:24:30
+ * @Description:
+ */
+
+class AppleStore {}
+
+const apple = new AppleStore()
+
+export default counter
